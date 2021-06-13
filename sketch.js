dc3549b657185cd44d4002fe296e56f12f59d0bf
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2021-06-14 01:07:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,16 +12,18 @@ function setup() {
   amplitude = new p5.Amplitude();
 }
 
+// shape attribute
 function draw() {
   background(2);
   fill('red')
+  // to get the volume or amplitude of the sound
   let level = amplitude.getLevel();
   let diam = map(level, 0, 1, 10, 400);
-  //ellipseMode(CENTER);
   ellipse(displayWidth / 2, displayHeight / 2,diam, diam);
   //ellipse(150,150,diam,diam);
 }
 
+// to play and stop the sound
 function mousePressed(event) {
   if(mySound.isPlaying()) {
     mySound.stop();
